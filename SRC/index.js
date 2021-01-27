787c88417532ceae0ed22b83b4001c851ddfcd92
Magali Garot <magali.garot@gmail.com>
2021-01-27 14:27:45 +0100

    test

---
+++ SRC/index.js
@@ -0,0 +1,23 @@
+console.log("hello world");
+
+// JSX, et c’est une extension syntaxique de JavaScriptJSX,
+// et c’est une extension syntaxique de JavaScript
+
+// import React from "react";
+// import { render } from "react-dom";
+// class HelloMessage extends React.Component {
+//     render() {
+//       return (
+//         <div>
+//           Hello {this.props.name}
+//         </div>
+//       );
+//     }
+//   }
+
+//   render(
+//     <HelloMessage name="Taylor" />,
+//     document.getElementById('hello-example')
+//   );
+
+// console.log("yo");
